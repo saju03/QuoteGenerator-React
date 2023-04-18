b8c4d01dbccb13fcb37caa3d14839fc99f6a6dc3
saju03 <sajusuresh03@gmail.com>
2023-04-18 16:59:30 +0530

    feature Homepage

--- src/components/quote/QuoteBody.jsx
+++ src/components/quote/QuoteBody.jsx
@@ -1,22 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './quotebody.css'
+import axios from 'axios'
 function QuoteBody() {
+    const [quote,setQuote] = useState({})
+    const [tags,setTags] = useState([])
+
+    const fetchData = ()=>{
+        try {
+         axios.get('https://api.quotable.io/random').then(({data})=>{
+            console.log(data);
+            setQuote(data)
+
+         })
+
+         axios.get('https://api.quotable.io/tags').then(({data})=>{
+            console.log(data);
+            setTags(data)
+         })
+    
+        } catch (error) {
+            console.log(error);
+        }
+       
+    }
+
+    const searchTag = (e)=>{
+        try {
+     
+
+        axios.get(`https://api.quotable.io/search/quotes?query=${e.target.value}`).then(({data})=>{
+           console.log(data);
+            if (data.count>0) {
+              setQuote(data.results[0])
+            }
+        
+        })
+        } catch (error) {
+            console.log(error);
+        }
+       
+
+    }
+    useEffect(()=>{
+        fetchData();
+    },[])
     return (
+        <>
         <div className='Body'>
             <div className='quote'>
                 <div className='message'>
-                    <span>alkjdflak asdaf asdf asdfasdafasdf asdfasdfa adsfaad adf</span>
+                    <span >{quote.content}</span>
                 </div>
 
                 <div className='atterbutes'>
-                    <span>-sdfasdjlflaksjdf</span>
+                    <span>-{quote.authorSlug}</span>
                     <button className='addTobook'>+</button>
-
                 </div>
 
             </div>
             
         </div>
+        <div className='actions'>
+            <div className='drop'>
+                <select className='dropbox' name="slug" onChange={searchTag}>
+                    <option></option>
+                   {tags.map((e)=>{
+                    return (<option key={e._id}>{e.slug}</option> )
+                   })}
+                </select>
+
+            </div>
+
+            <div className='next'>
+
+            <button onClick={fetchData} className='nextBtn'>next</button>
+            </div>
+
+        </div>
+        
+        </>
     )
 }
 
